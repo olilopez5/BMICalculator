04027a6965a04603a6f4c2d756b5e1306b9c8fa9
olilopez5 <olilopez5@outlook.es>
2025-02-21 12:30:48 +0100

    First concept of the app

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.example.bmicalculator
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var heightEditText : EditText
+    lateinit var weightEditText : EditText
+    lateinit var calculateButton: Button
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        //R. Res Vistas
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        heightEditText = findViewById(R.id.heightEditText)
+        weightEditText = findViewById(R.id.weightEditText)
+        calculateButton = findViewById(R.id.calculateButton)
+
+
+        calculateButton.setOnClickListener {
+            println("Altura : " + heightEditText.text.toString())
+            println("Peso : " + heightEditText.text.toString())
+
         }
     }
 }
