2495008f0dcc3f2ed34e437c77fb853426b6df99
olilopez5 <olilopez5@outlook.es>
2025-02-24 13:33:13 +0100

    Second view

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.bmicalculator
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.slider.Slider
 import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
@@ -15,10 +17,10 @@ class MainActivity : AppCompatActivity() {
     lateinit var heightEditText : EditText
     lateinit var weightEditText : EditText
     lateinit var calculateButton: Button
-    lateinit var resultIMC : TextView
+    lateinit var resultBMI : TextView
 
-
-
+    lateinit var edadSlider : Slider
+    lateinit var edadTextView: TextView
 
 
 
@@ -37,16 +39,36 @@ class MainActivity : AppCompatActivity() {
         heightEditText = findViewById(R.id.heightEditText)
         weightEditText = findViewById(R.id.weightEditText)
         calculateButton = findViewById(R.id.calculateButton)
-        resultIMC = findViewById(R.id.resultIMC)
+        resultBMI = findViewById(R.id.resultBMI)
 
+        edadTextView = findViewById(R.id.edadTextView)
+        edadSlider = findViewById(R.id.edadSlider)
+
+
+        edadSlider.addOnChangeListener { slider, value, fromUser ->
+            edadTextView.text = "${value.toInt()}"
+        }
+
+        
 
         calculateButton.setOnClickListener {
          val height = heightEditText.text.toString().toFloat()
             val weight = weightEditText.text.toString().toFloat()
 
             val result = weight / ((height/100).pow(2))
-            resultIMC.text = "IMC: $result %"
+            resultBMI.text = "$result %"
+
+            val category = when {
+                result < 18.5 -> "Bajo peso"
+                result in 18.5..24.9 -> "Peso normal"
+                result in 25.0..29.9 -> "Sobrepeso"
+                result in 30.0..34.9 -> "Obesidad (Grado 1)"
+                result in 35.0..39.9 -> "Obesidad (Grado 2)"
+                result >= 40.0 -> "Obesidad (Grado 3)"
+                else -> "Valor inválido"
+            }
 
         }
+
     }
 }
